02568f82fa93c6b58c536a01896c90534f668b57
rr34 <nate@rr34.us>
2022-11-07 04:11:11 -0500

    I can write to the exif UserComment successfully with text by marking then parsing the text. Text-only. This is sufficient for everything except reading exif of raw images. If I want to do that, I have to really bind to exiv2, which would take a while. Conclusion: use exiv2 to store text data in UserComment, but continue to use PIL to read exif.

--- awim/exifedit.py
+++ awim/exifedit.py
@@ -1,7 +1,18 @@
 import subprocess
 
+def set_UserComment(filepath):
+    command1 = '-M'
+    command2 = 'set Exif.Photo.UserComment '
+    user_comment = 'commentstart' + 'i wrote a user comment here\nso you should be super-impressed' + 'commentend'
+    success = True
+    try:
+        subprocess.call(["exiv2", command1+command2+user_comment, filepath])
+    except subprocess.CalledProcessError:
+        success = False
+
+    return success
+
 def read_UserComment(filepath):
-    exiv2_args = "-pt --grep UserComment"
     try:
         user_comment = subprocess.check_output(["exiv2", "-pt", "--grep" , "UserComment", filepath])
     except subprocess.CalledProcessError as err:
@@ -10,7 +21,32 @@ def read_UserComment(filepath):
     if isinstance(user_comment, bytes):
         user_comment = user_comment.decode()
 
-    return user_comment
+    commentstart = user_comment.find("commentstart")
+    commentend = user_comment.find("commentend")
+    comment_parsed = user_comment[commentstart+12:commentend]
 
-a = read_UserComment('LN101821.jpg')
-print(a)
+
+    return comment_parsed
+
+def read_exif_field(filepath):
+    try:
+        field_value = subprocess.check_output(["exiv2", "-K" , "Exif.Image.XResolution", filepath])
+    except subprocess.CalledProcessError as err:
+        field_value = err
+
+    if isinstance(field_value, bytes):
+        field_value_decoded = field_value.decode()
+
+    return field_value
+
+filepath = 'LN101821.jpg'
+
+a = set_UserComment(filepath)
+
+if a:
+    b = read_UserComment(filepath)
+
+c = read_exif_field(filepath)
+
+print(c)
+print(type(c))
