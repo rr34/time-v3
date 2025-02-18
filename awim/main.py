748377fdbad3be7849327ea7294098b840e23f46
Nathan Ruffing <nate@rr34.us>
2025-02-18 06:53:56 -0500

    The eagle has landed. The clock is talking to the awim API.

--- awim/main.py
+++ awim/main.py
@@ -1,16 +1,28 @@
 import uvicorn
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 import json
 import actions, awimlib, metadata_tools
 
 app = FastAPI()
 
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST", "GET"],
+    allow_headers=["*"],
+)
+
+
 @app.post('/testpost')
-async def test(request: Request):
+async def testpost(request: Request):
+    print('get here?')
     try:
-        whatsthis = request.body()
+        whatsthis = await request.body()
         print(whatsthis)
-        data_received = json.loads(await request.body())
+        data_received = json.loads(request.body())
         print(data_received)
         awimTag = await request.json()
         print(awimTag)
@@ -18,21 +30,21 @@ async def test(request: Request):
         print('some error on the post request attempt')
 
 
-@app.get("/testget")
-async def test(request: Request):
-    try:
-        data_received_dict = request.query_params._dict
-        print(data_received_dict['foo'])
-        print(data_received_dict['foo2'])
-        data_received = request.query_params.get('foo')
-        print(data_received)
-        data_received = request.query_params.get('foo2')
-        print(data_received)
-        # this works with http://localhost:8080/testget?foo=bar&foo2=bar2
-    except:
-        print('some error on the get request')
+# @app.get("/testget/")
+# async def testget(request: Request):
+#     try:
+#         data_received_dict = request.query_params._dict
+#         print(data_received_dict['foo'])
+#         print(data_received_dict['foo2'])
+#         data_received = request.query_params.get('foo')
+#         print(data_received)
+#         data_received = request.query_params.get('foo2')
+#         print(data_received)
+#         # this works with http://localhost:8000/testget?foo=bar&foo2=bar2
+#     except:
+#         print('some error on the get request')
     
-    return data_received_dict
+#     return data_received_dict
 
 if __name__ == '__main__':
-    uvicorn.run(app, port=8080)
+    uvicorn.run(app)
