5292110b7f6fd9898f7bfe82fbfa1d14664a2689
Nathan Ruffing <nate@rr34.us>
2025-02-18 06:07:48 -0500

    main.py works as a server and succesfully responds to GET request. Next is to work on POST request from the clock.

--- awim/main.py
+++ awim/main.py
@@ -5,17 +5,34 @@ import actions, awimlib, metadata_tools
 
 app = FastAPI()
 
-@app.post("/test/")
+@app.post('/testpost')
 async def test(request: Request):
     try:
+        whatsthis = request.body()
+        print(whatsthis)
         data_received = json.loads(await request.body())
         print(data_received)
         awimTag = await request.json()
         print(awimTag)
     except:
-        print('some error')
+        print('some error on the post request attempt')
+
+
+@app.get("/testget")
+async def test(request: Request):
+    try:
+        data_received_dict = request.query_params._dict
+        print(data_received_dict['foo'])
+        print(data_received_dict['foo2'])
+        data_received = request.query_params.get('foo')
+        print(data_received)
+        data_received = request.query_params.get('foo2')
+        print(data_received)
+        # this works with http://localhost:8080/testget?foo=bar&foo2=bar2
+    except:
+        print('some error on the get request')
     
-    return {'message': 'Hello World'}
+    return data_received_dict
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080)
