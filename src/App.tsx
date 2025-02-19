a3499101cceef721de90d14258e3a7ca2868e896
Nathan Ruffing <nate@rr34.us>
2025-02-19 10:23:51 -0500

    Successfully fetches data now. Working on fetching the correct data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import ClockScreen from './components/ClockScreen';
+
 import './App.css';
 
 function App() {
-  const [PostId, setPostId] = useState(11111)
+  const [PostId, setPostId] = useState(Array)
   useEffect(() => {
       // POST request using fetch inside useEffect React hook
       const requestOptions = {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ requestType: 'get time strings', moments: ['2025-02-19T11:19Z', '2025-02-19T11:20Z']})
+          body: JSON.stringify(['2025-02-19T11:19Z', '2025-02-19T11:20Z'])
       };
-      fetch('http://localhost:8000/testpost', requestOptions)
-          .then(response => console.log(response.json()))
-          .then(data => console.log(data["id"]));
+      fetch('http://localhost:8000/timestrings', requestOptions)
+          .then(response => {
+            return response.json()
+          })
+          .then(data => console.log(JSON.parse(data)['id']));
   // empty dependency array means this effect will only run once (like componentDidMount in classes)
   }, []);
 
