8e794a63b1a6a15dcbaf7e2c13a7fb55ea2a1131
Nathan Ruffing <nate@rr34.us>
2025-02-19 07:06:07 -0500

    Made a lot of notes on what data needs to be requested and in what format. Received response from API, but cannot yet access it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import ClockScreen from './components/ClockScreen';
 import './App.css';
 
 function App() {
+  const [PostId, setPostId] = useState(11111)
   useEffect(() => {
       // POST request using fetch inside useEffect React hook
       const requestOptions = {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ title: 'React Hooks POST Request Example' })
+          body: JSON.stringify({ requestType: 'get time strings', moments: ['2025-02-19T11:19Z', '2025-02-19T11:20Z']})
       };
       fetch('http://localhost:8000/testpost', requestOptions)
-          .then(response => response.json())
-          // .then(data => setPostId(data.id));
-  
+          .then(response => console.log(response.json()))
+          .then(data => console.log(data["id"]));
   // empty dependency array means this effect will only run once (like componentDidMount in classes)
   }, []);
 
   return (
     <div>
-      <ClockScreen />
+      {/* <ClockScreen /> */}
+      <p>{PostId}</p>
     </div>
   );
 }
