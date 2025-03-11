xang calculation in camera.py is wrong. Need to correct. Updated diagrams a lot and named Figure 1.PSD

awim/astromath.py
@@ -172,6 +172,13 @@ def calculate_astro_newfullmoon(moment_now, discretize=150):
     return newmoon_time, newmoon_angle, fullmoon_time, fullmoon_angle
 
 
+def calculate_astro_moonphaseangle(moments):
+    moments_astropy = Time(moments)
+    moon_phase = astroplan.moon_phase_angle(moments_astropy).to_value()
+
+    return moon_phase
+
+
 # dictionary of objects, get data for objects at moments, return dictionary
 def calculate_astro_data(moments, earth_latlng, celestial_objects_list):
     celestial_objs_dictionary = {}
@@ -179,6 +186,7 @@ def calculate_astro_data(moments, earth_latlng, celestial_objects_list):
     img_astropy_times = Time(moments)
     img_astropy_altazframes = AltAz(obstime=img_astropy_times, location=img_astropy_location)
     bodies = ['moon', 'mercury', 'venus', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune']
+    # TODO? With long lists of stars like I usually have, this would be more efficient doing ~20 moment iterations, each with a large array of star RA, Dec rather than 500+ star iterations, each with a small array of moments.
     for celestial_object in celestial_objects_list:
         if isinstance(celestial_object, str):
             if celestial_object == 'sun':
@@ -200,7 +208,7 @@ def calculate_astro_data(moments, earth_latlng, celestial_objects_list):
         if object_type != 'star':
             celestial_object_distances = object_SkyCoords.distance.au
         else:
-            celestial_object_distances = np.full(moments.size, 0)
+            celestial_object_distances = np.zeros(moments.size)
 
         astro_data = np.zeros((moments.size, 5))
         astro_data[:,0] = celestial_object_ras
@@ -221,7 +229,7 @@ def calculate_astro_data(moments, earth_latlng, celestial_objects_list):
 
 
 # to display the moon partially illuminated I need the angle it appears to be illuminated.
-# return degrees. straight down = sun stright below moon = 0°. (+) angle is CCW = illum up the right side. (-) angle is CW = illum up the left side.
+# return degrees. straight down = sun straight below moon = 0°. (+) angle is CCW = illum up the right side. (-) angle is CW = illum up the left side.
 # see diagrams for variable meanings.
 def calculate_astro_moon_brightsidedirection(moon_azalts_deg, sun_azalts_deg):
     # moon_azalts[:,2] = np.where(np.greater_equal(moon_azalts[:,2], 0), moon_azalts[:,2], 0)

awim/clockactions.py
@@ -22,6 +22,7 @@ def get_celestialinphoto(awim_dict, momentsarray, elevation, requestlist):
     momentsarray = np.array([np.datetime64(moment) for moment in momentsarray])
     location = awim_dict['awim Location Coordinates']
     celestial_bodies = []
+    # The following loop just creates the expanded list of bodies. Within solar system just get a name because RA, Dec has to be calculated. Outside solar system (stars) are a tuple of name with the RA, Dec given.
     for request in requestlist:
         if request == 'sun':
             celestial_bodies.append('sun')
@@ -44,12 +45,37 @@ def get_celestialinphoto(awim_dict, momentsarray, elevation, requestlist):
 
     # bodies in the image dictionary generated here outside the awimlib functions because there is no commonality among the bodies in image for efficiency
     bodies_image_dict = {}
+    padding_percent = 10
+    inimage_threshold = 2
     for key, value in bodies_astro_dict.items():
-        azarts = value[:,3:5]
-        bodies_xyangs = awimlib.azarts_to_xyangs(awim_dict, azarts)
-        bodies_inimage = awimlib.xyangs_inimage(awim_dict, bodies_xyangs, padding_percent=10)
-        body_data = np.zeros((momentsarray.size, 3))
-        body_data[:,0] = bodies_inimage
-        # body_data[:,1:3] = pxs
+        body_azarts = value[:,3:5]
+        bodies_xyangs = awimlib.azarts_to_xyangs(awim_dict, body_azarts)
+        body_inimage = awimlib.xyangs_inimage(awim_dict, bodies_xyangs, padding_percent=padding_percent)
+        if body_inimage.sum() >= inimage_threshold:
+            print(key)
+            bodies_pxs = awimlib.xyangs_to_pxs(awim_dict, bodies_xyangs)
+            if key not in ['sun', 'moon']:
+                body_data = np.zeros((momentsarray.size, 4))
+            elif key == 'sun':
+                body_data = np.zeros((momentsarray.size, 6))
+                body_data[:,4:6] = body_azarts
+            elif key == 'moon':
+                body_data = np.zeros((momentsarray.size, 8))
+                body_data[:,4:6] = body_azarts
+                phase_angle = astromath.calculate_astro_moonphaseangle(momentsarray)
+                body_data[:,6] = phase_angle
+                sun_azarts = bodies_astro_dict['sun'][:,3:5]
+                brightside_direction = astromath.calculate_astro_moon_brightsidedirection(body_azarts, sun_azarts)
+                body_data[:,7] = brightside_direction
 
-        bodies_image_dict[key] = body_data
+            body_data[:,0] = body_inimage
+            body_data[:,1:3] = bodies_pxs
+            body_data[:,3] = value[:,2]
+
+            bodies_image_dict[key] = body_data
+    
+    bodies_total = len(bodies_astro_dict)
+    bodiescount_inimage = len(bodies_image_dict)
+    print(f'Total bodies: {bodies_total}. Bodies in image during period: {bodiescount_inimage}. Percent in image: {bodiescount_inimage/bodies_total * 100}')
+
+    return bodies_astro_dict, bodies_image_dict