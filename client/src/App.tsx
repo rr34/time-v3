eb94c3c93d0bc736019fdc4d11f7e5857cd5eec9
Nathan Ruffing <nate@rr34.us>
2025-05-08 10:16:45 -0400

    Back in the fight after several weeks. Clock strings work. Need to move clock strings to a separate component while I work on the image.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import ClockStrings from "./components/ClockStrings";
 
 import ClockScreen from './components/ClockScreen';
-import imageAWIM from './assets/clock_images/timhouse20220410 - NL100550.json'; // todonext: this needs to be moved to the backend with the images so the images can be selected dynamically by the server.
+// import imageAWIM from './assets/clock_images/timhouse20220410 - NL100550.json'; // todonext: this needs to be moved to the backend with the images so the images can be selected dynamically by the server.
 
 export interface ClockTimeObj {
   currenttime: Date;
@@ -26,87 +26,87 @@ function App() {
   const clockMSL = 280;
 
   // initialize time state object
-  let addhours = 0;
-  let newdate = new Date(Date.now() + addhours*1000*60*60);
+  const addhours = 0;
+  const newdate = new Date(Date.now() + addhours*1000*60*60);
   const [ClockTimeObj, setClockTimeObj] = useState({ currenttime: newdate, sunindex: 0 });
   
   // initialize daily events object
   const [DailyEventsObj, setDailyEventsObj] = useState({ sundaily: [new Date()], moondaily: [new Date()], nearestnew: newdate, nearestnewangle: 0, nearestfull: newdate, nearestfullangle: 0 });
 
   // update the clock strings every second. todo: fix this because it causes the entire app to reload twice every second when it fires.
-  // useEffect(() => {
-  //     const intervalID = setInterval(() => {
-  //         let addhours = 0;
-  //         let newdate = new Date(Date.now() + addhours*1000*60*60);
-  //         let sunindex = DailyEventsObj.sundaily.findIndex((date, i) => newdate < date);
-  //         setClockTimeObj({ currenttime: newdate, sunindex: sunindex })
-  //     }, 1*1000);
+  useEffect(() => {
+      const intervalID = setInterval(() => {
+          let addhours = 0;
+          let newdate = new Date(Date.now() + addhours*1000*60*60);
+          let sunindex = DailyEventsObj.sundaily.findIndex((date, i) => newdate < date);
+          setClockTimeObj({ currenttime: newdate, sunindex: sunindex })
+      }, 1*1000);
 
-  // return () => clearInterval(intervalID);
-  // });
+  return () => clearInterval(intervalID);
+  });
 
   // don't need the following until I start animating SVG
-  let momentscount: number = 20;
-  let stepminutes: number = 15;
-  let stepsbefore: number = 4;
-  let nowdate: number = Date.now();
-  let momentsarray: string[] = [];
+  const momentscount: number = 20;
+  const stepminutes: number = 15;
+  const stepsbefore: number = 4;
+  const nowdate: number = Date.now();
+  const momentsarray: string[] = [];
   for (let i=-stepsbefore; i<momentscount-stepsbefore; i++) { // generates times from an hour prior to CurrentTime until 4 hours after CurrentTime
-    let idate = new Date(nowdate + i * stepminutes*1000*60);
+    const idate = new Date(nowdate + i * stepminutes*1000*60);
     momentsarray.push(idate.toISOString());
   }
   const [NowMoments, setNowMoments] = useState<string[]>(momentsarray);
 
   // todo: this is firing twice on initialization and making a duplicate request to the API
-  // useEffect(() => {
-  //     const requestOptions = {
-  //         method: 'POST',
-  //         headers: { 'Content-Type': 'application/json' },
-  //         body: JSON.stringify({ location: clockLatLong, elevation: clockMSL, currenttime: ClockTimeObj.currenttime })
-  //     };
-  //     fetch('http://localhost:8000/getevents', requestOptions)
-  //         .then(response => {
-  //           return response.json()
-  //         })
-  //         .then(data => {
-  //           let sundaily_strings: string[] = JSON.parse(data)['sundaily'];
-  //           let sundaily_dates: Date[] = [];
-  //           sundaily_strings.forEach(element => {
-  //             sundaily_dates.push(new Date(element));
-  //           });
-            
-  //           let moonDaily_strings: string[] = JSON.parse(data)['moondaily'];
-  //           let moondaily_dates: Date[] = [];
-  //           moonDaily_strings.forEach(element => {
-  //             moondaily_dates.push(new Date(element));
-  //           });
-  //           let newmoon_time: string = JSON.parse(data)['newmoon time'];
-  //           let newmoon_angle: number = Math.round(JSON.parse(data)['newmoon angle']*100) / 100;
-  //           let fullmoon_time: string = JSON.parse(data)['fullmoon time'];
-  //           let fullmoon_angle: number = Math.round(JSON.parse(data)['fullmoon angle']*100) / 100;
-  //           setDailyEventsObj({ sundaily: sundaily_dates, moondaily: moondaily_dates, nearestnew: new Date(newmoon_time), nearestnewangle: newmoon_angle, nearestfull: new Date(fullmoon_time), nearestfullangle: fullmoon_angle })
-  //         })
-  // }, []); // I can put variables in the dependency array and this will run whenever the variables change value.
-
   useEffect(() => {
       const requestOptions = {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ awim: imageAWIM, momentsarray: NowMoments, elevation: clockMSL, requestlist: ['stars', 'sun', 'moon', 'planets'], returnastro: 'true' })
+          body: JSON.stringify({ location: clockLatLong, elevation: clockMSL, currenttime: ClockTimeObj.currenttime })
       };
-      fetch('http://localhost:8000/celestialinphoto', requestOptions)
+      fetch('http://localhost:8000/getevents', requestOptions)
           .then(response => {
             return response.json()
           })
           .then(data => {
-            console.log('do here like above to retrieve the data')
+            let sundaily_strings: string[] = JSON.parse(data)['sundaily'];
+            let sundaily_dates: Date[] = [];
+            sundaily_strings.forEach(element => {
+              sundaily_dates.push(new Date(element));
+            });
+            
+            let moonDaily_strings: string[] = JSON.parse(data)['moondaily'];
+            let moondaily_dates: Date[] = [];
+            moonDaily_strings.forEach(element => {
+              moondaily_dates.push(new Date(element));
+            });
+            let newmoon_time: string = JSON.parse(data)['newmoon time'];
+            let newmoon_angle: number = Math.round(JSON.parse(data)['newmoon angle']*100) / 100;
+            let fullmoon_time: string = JSON.parse(data)['fullmoon time'];
+            let fullmoon_angle: number = Math.round(JSON.parse(data)['fullmoon angle']*100) / 100;
+            setDailyEventsObj({ sundaily: sundaily_dates, moondaily: moondaily_dates, nearestnew: new Date(newmoon_time), nearestnewangle: newmoon_angle, nearestfull: new Date(fullmoon_time), nearestfullangle: fullmoon_angle })
           })
   }, []);
 
+  // useEffect(() => {
+  //     const requestOptions = {
+  //         method: 'POST',
+  //         headers: { 'Content-Type': 'application/json' },
+  //         body: JSON.stringify({ awim: imageAWIM, momentsarray: NowMoments, elevation: clockMSL, requestlist: ['stars', 'sun', 'moon', 'planets'], returnastro: 'true' })
+  //     };
+  //     fetch('http://localhost:8000/celestialinphoto', requestOptions)
+  //         .then(response => {
+  //           return response.json()
+  //         })
+  //         .then(data => {
+  //           console.log('do here like above to retrieve the data')
+  //         })
+  // }, []);
+
   return (
     <div>
-      {<ClockScreen />}
-      {/* <ClockStrings cto={ClockTimeObj} deo={DailyEventsObj} /> */}
+      {/* <ClockScreen /> */}
+      { <ClockStrings cto={ClockTimeObj} deo={DailyEventsObj} />}
     </div>
   );
 }
