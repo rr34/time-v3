ed12a9558f4f00e6f3b03a7a38f4aefb7720194f
Nathan Ruffing <nate@rr34.us>
2025-06-21 08:28:25 -0400

    Small changes.

--- awim/main.py
+++ awim/main.py
@@ -36,15 +36,18 @@ async def celestialinphoto(request: Request):
     except:
         print('some error on the post request attempt')
 
-    any_awim = json.loads(request_dict['awims_list'][0]['awimTag'])
-    astro_dict, astro_dict_lists = clockactions.get_astrodata(any_awim, request_dict['momentsarray'], request_dict['requestlist'])
+    awims_dict = request_dict['awims_dict'] # keys are basenames
+    first_key = next(iter(awims_dict)) # first basename
+    any_awim = awims_dict[first_key]['awimTag']
+    astro_dict, astro_dict_list_type = clockactions.get_astrodata(any_awim, request_dict['momentsarray'], request_dict['requestlist'])
+    # (the lists version of the dictionary is just where the numpy arrays have been converted to standard python lists)
 
     bodies_inimage_dicts = {}
-    for item in request_dict['awims_list']:
-        bodies_inimage_dict = clockactions.get_celestialinphoto(json.loads(item['awimTag']), request_dict['momentsarray'], astro_dict)
-        bodies_inimage_dicts[item['Basename']] = bodies_inimage_dict
+    for basename, item in awims_dict.items():
+        bodies_inimage_dict = clockactions.get_celestialinphoto(item['awimTag'], request_dict['momentsarray'], astro_dict)
+        bodies_inimage_dicts[basename] = bodies_inimage_dict
 
-    response_dict = {'astro dict': astro_dict_lists, 'bodies in images dicts': bodies_inimage_dicts}
+    response_dict = {'astro dict': astro_dict_list_type, 'bodies in images dicts': bodies_inimage_dicts}
 
     return response_dict
 
