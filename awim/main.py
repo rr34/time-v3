f14f8ca09fdb9ef194db1f2d16d6a48ac684f448
Nathan Ruffing <nate@rr34.us>
2025-06-22 17:21:41 -0400

    Set up to serve remotely using environment variables.

--- awim/main.py
+++ awim/main.py
@@ -52,4 +52,6 @@ async def celestialinphoto(request: Request):
     return response_dict
 
 if __name__ == '__main__':
-    uvicorn.run(app)
+    host = os.getenv("HOST", "127.0.0.1")   # fallback to localhost
+    port = int(os.getenv("PORT", 8000))     # fallback to 8000d
+    uvicorn.run(app, host=host, port=port)
