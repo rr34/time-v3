5ff5f4f90e425c113d30aa0179582cfd5f23ef57
Nathan Ruffing <nate@rr34.us>
2025-06-23 23:35:00 -0400

    Improved error handling in celestialinphotos.

--- awim/main.py
+++ awim/main.py
@@ -36,33 +36,34 @@ async def getevents(request: Request):
 async def celestialinphoto(request: Request):
     try:
         request_dict = await request.json()
-    except:
-        print('some error on the post request attempt')
+        awims_dict = request_dict['awims_dict'] # keys are basenames
+        first_key = next(iter(awims_dict)) # first basename
+        any_awim = awims_dict[first_key]['awimTag']
+        location = any_awim['awim Location Coordinates']
+        if not any_awim['awim Location MSL']:
+            if any_awim['awim Location Terrain Elevation'] and any_awim['awim Location AGL']:
+                elevation = any_awim['awim Location Terrain Elevation'] and any_awim['awim Location AGL'] # elevation is used for events because affects horizon
 
-    awims_dict = request_dict['awims_dict'] # keys are basenames
-    first_key = next(iter(awims_dict)) # first basename
-    any_awim = awims_dict[first_key]['awimTag']
-    location = any_awim['awim Location Coordinates']
-    if not any_awim['awim Location MSL']:
-        if any_awim['awim Location Terrain Elevation'] and any_awim['awim Location AGL']:
-            elevation = any_awim['awim Location Terrain Elevation'] and any_awim['awim Location AGL'] # elevation is used for events because affects horizon
+        MagRankAllMax = request_dict['MagRankAllMax']
+        LatDec_filter = request_dict['LatDecFilter']
+        if LatDec_filter:
+            LatDec_filter = location[0] # equals the latitude, which limits the stars you can see.
+        
+        astro_dict, astro_dict_list_type = clockactions.get_astrodata(location, request_dict['momentsarray'], request_dict['requestlist'], MagRankAllMax, LatDec_filter)
+        # (the lists version of the dictionary is just where the numpy arrays have been converted to standard python lists)
 
-    MagRankAllMax = request_dict['MagRankAllMax']
-    LatDec_filter = request_dict['LatDecFilter']
-    if LatDec_filter:
-        LatDec_filter = location[0] # equals the latitude, which limits the stars you can see.
-    
-    astro_dict, astro_dict_list_type = clockactions.get_astrodata(location, request_dict['momentsarray'], request_dict['requestlist'], MagRankAllMax, LatDec_filter)
-    # (the lists version of the dictionary is just where the numpy arrays have been converted to standard python lists)
+        bodies_inimage_dicts = {}
+        for basename, item in awims_dict.items():
+            bodies_inimage_dict = clockactions.get_celestialinphoto(item['awimTag'], request_dict['momentsarray'], astro_dict)
+            bodies_inimage_dicts[basename] = bodies_inimage_dict
 
-    bodies_inimage_dicts = {}
-    for basename, item in awims_dict.items():
-        bodies_inimage_dict = clockactions.get_celestialinphoto(item['awimTag'], request_dict['momentsarray'], astro_dict)
-        bodies_inimage_dicts[basename] = bodies_inimage_dict
+        response_dict = {'astro dict': astro_dict_list_type, 'bodies in images dicts': bodies_inimage_dicts}
 
-    response_dict = {'astro dict': astro_dict_list_type, 'bodies in images dicts': bodies_inimage_dicts}
+        return response_dict
+    except Exception as e:
+        print(f'Error in /celestialinphotos: {e}')
+        return {"error": str(e)} # return a json response with CORS headers intact
 
-    return response_dict
 
 if __name__ == '__main__':
     host = os.getenv("HOST", "127.0.0.1")   # fallback to localhost
